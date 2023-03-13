ec6a32a20cf3fba9b0a4d9e14e44c4e74bbc40d6
sajad <salimzadehsajad@gmail.com>
2023-03-13 16:18:03 +0330

    key filter implemented

---
+++ src/app/directives/key-filter/key-filter.directive.ts
@@ -0,0 +1,118 @@
+import {Directive, ElementRef, HostListener, Input, NgModule} from "@angular/core";
+import {CommonModule} from "@angular/common";
+
+const intRegex = /^[0-9]$/;
+
+@Directive({
+  selector: '[devKeyFilter]'
+})
+export class DevKeyFilterDirective {
+  @Input('devKeyFilter') devKeyFilter!: 'p-int' | 'int' | 'n-int' | 'p-num' | 'num' | 'n-num' | 'hex' | 'email' | 'alpha' | 'alphnum' | string;
+
+  @HostListener('keydown', ['$event'])
+  onKeyDown(e: KeyboardEvent) {
+    const target = e.target;
+    if (!(target instanceof HTMLInputElement)) return;
+    if (e.key.length != 1) return;
+
+    switch (this.devKeyFilter) {
+      case 'p-int':
+        if (target.value.length == 0 && /^\+$/.test(e.key)) {
+          return;
+        } else if (!intRegex.test(e.key)) {
+          e.preventDefault();
+        }
+        break;
+      case 'int':
+        if (target.value.length == 0 && /^[+-]$/.test(e.key)) {
+          return;
+        } else if (!intRegex.test(e.key)) {
+          e.preventDefault();
+        }
+        break;
+      case 'n-int':
+        if (target.value.length == 0) {
+          if(!/^-$/.test(e.key)) {
+            e.preventDefault();
+          }
+        } else if (!intRegex.test(e.key)) {
+          e.preventDefault();
+        }
+        break;
+      case 'p-num':
+        if (target.value.length == 0 && /^\+$/.test(e.key)) {
+          return;
+        } else if (target.value.includes('.')) {
+          if (!intRegex.test(e.key)) {
+            e.preventDefault();
+          }
+        } else if (!/^\.$/.test(e.key) && !intRegex.test(e.key)) {
+          e.preventDefault();
+        }
+        break;
+      case 'num':
+        if (target.value.length == 0 && /^[+-]$/.test(e.key)) {
+          return;
+        } else if (target.value.includes('.')) {
+          if (!intRegex.test(e.key)) {
+            e.preventDefault();
+          }
+        } else if (!/^\.$/.test(e.key) && !intRegex.test(e.key)) {
+          e.preventDefault();
+        }
+        break;
+      case 'n-num':
+        if (target.value.length == 0) {
+          if (!/^-$/.test(e.key)) {
+            e.preventDefault();
+          }
+        } else if (target.value.includes('.')) {
+          if (!intRegex.test(e.key)) {
+            e.preventDefault();
+          }
+        } else if (!/^\.$/.test(e.key) && !intRegex.test(e.key)) {
+          e.preventDefault();
+        }
+        break;
+      case 'hex':
+        if (!e.key.match(/^[0-9a-hA-H]$/)) {
+          e.preventDefault();
+        }
+        break;
+      case 'email':
+        if (!e.key.match(/^[0-9]$/)) {
+          e.preventDefault();
+        }
+        break;
+      case 'alpha':
+        if (!e.key.match(/^[a-zA-Z]$/)) {
+          e.preventDefault();
+        }
+        break;
+      case 'alphanum':
+        if (!e.key.match(/^[a-zA-Z0-9]$/)) {
+          e.preventDefault();
+        }
+        break;
+    }
+  }
+
+
+  constructor(private elementRef: ElementRef<HTMLInputElement>) {
+
+  }
+}
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    DevKeyFilterDirective
+  ],
+  exports: [
+    DevKeyFilterDirective
+  ]
+})
+export class KeyFilterModule {
+}
