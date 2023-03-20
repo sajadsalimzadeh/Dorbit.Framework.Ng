44825721d67e1fa6fe2802edc82b991bca1eb690
sajad <salimzadehsajad@gmail.com>
2023-03-20 23:45:57 +0330

    improve docs panel

--- src/app/docs/index.component.ts
+++ src/app/docs/index.component.ts
@@ -8,8 +8,18 @@ import {NavigationEnd, Router, RouterEvent} from "@angular/router";
 })
 export class IndexComponent implements OnInit {
   menus = [
-    {text: 'Button', link: 'button', icon: 'far fa-computer-mouse'},
     {text: 'Color Pallet', link: 'color-pallet-pallet', icon: 'far fa-palette'},
+    {text: 'Button', link: 'button', icon: 'far fa-computer-mouse'},
+    {text: 'Timeline', link: 'timeline', icon: 'far fa-timeline'},
+    {text: 'Data Table', link: 'data-table', icon: 'far fa-table'},
+    {text: 'Paginator', link: 'paginator', icon: 'far fa-ellipsis-stroke'},
+    {text: 'Progress Bar', link: 'progress-bar', icon: 'far fa-bars-progress'},
+    {text: 'Scroll top', link: 'scroll-top', icon: 'far fa-up-down'},
+    {text: 'Skeleton', link: 'skeleton', icon: 'far fa-loader'},
+    {text: 'Tag', link: 'tag', icon: 'far fa-tag'},
+    {text: 'Message', link: 'message', icon: 'far fa-envelope'},
+    {text: 'Dialog', link: 'dialog', icon: 'far fa-rectangle-history'},
+    {text: 'Tooltip', link: 'tooltip', icon: 'far fa-message-middle-top'},
     {
       text: 'Forms', icon: 'far fa-input-text', children: [
         {text: 'Input', link: 'forms/input', icon: 'far fa-input-text'},
@@ -26,16 +36,6 @@ export class IndexComponent implements OnInit {
         {text: 'Password', link: 'forms/password', icon: 'far fa-key'},
       ]
     },
-    {text: 'Timeline', link: 'timeline', icon: 'far fa-timeline'},
-    {text: 'Data Table', link: 'data-table', icon: 'far fa-table'},
-    {text: 'Paginator', link: 'paginator', icon: 'far fa-ellipsis-stroke'},
-    {text: 'Progress Bar', link: 'progress-bar', icon: 'far fa-bars-progress'},
-    {text: 'Scroll top', link: 'scroll-top', icon: 'far fa-up-down'},
-    {text: 'Skeleton', link: 'skeleton', icon: 'far fa-loader'},
-    {text: 'Tag', link: 'tag', icon: 'far fa-tag'},
-    {text: 'Message', link: 'message', icon: 'far fa-envelope'},
-    {text: 'Dialog', link: 'dialog', icon: 'far fa-rectangle-history'},
-    {text: 'Tooltip', link: 'tooltip', icon: 'far fa-message-middle-top'},
   ];
   activeMenu: any;
 
