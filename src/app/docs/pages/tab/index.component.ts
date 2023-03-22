5f1573b6f0474548be2110c2df73df8d6fb10ee4
sajad <salimzadehsajad@gmail.com>
2023-03-22 22:41:36 +0430

    implement tab docs

---
+++ src/app/docs/pages/tab/index.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'doc-tab',
+  templateUrl: './index.component.html',
+  styleUrls: ['./index.component.scss']
+})
+export class IndexComponent {
+}
