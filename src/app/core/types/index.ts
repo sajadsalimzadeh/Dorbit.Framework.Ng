ed943451806d6d0597cc87a70848c8ed5701c631
sajad <salimzadehsajad@gmail.com>
2023-03-24 13:33:55 +0430

    add dark theme and select theme control in docs

--- src/app/core/types/index.ts
+++ src/app/core/types/index.ts
@@ -10,3 +10,4 @@ export type Positions = PositionsCross | PositionsCorner | 'middle-center';
 export type PositionsCross = 'middle-start' | 'middle-end' | 'top-center' | 'bottom-center';
 export type PositionsCorner = 'top-start' | 'top-end' | 'bottom-start' | 'bottom-end';
 export type Orientation = 'vertical' | 'horizontal';
+export type Themes = 'default-light' | 'default-dark';
