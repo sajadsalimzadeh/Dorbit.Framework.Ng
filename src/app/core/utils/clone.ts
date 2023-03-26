48122699787693e815fff24d6f901f5a362d879c
sajad <salimzadehsajad@gmail.com>
2023-03-26 14:33:35 +0330

    add clone utils

---
+++ src/app/core/utils/clone.ts
@@ -0,0 +1,31 @@
+
+export function clone(obj: any, level: number = 6) {
+    if (level < 0) return null;
+
+    if (!obj || typeof obj !== 'object') {
+        return obj;
+    }
+
+
+    if (Array.isArray(obj)) {
+        return cloneArray(obj, level - 1);
+    }
+
+    return cloneObject(obj, level -1);
+}
+
+export function cloneArray(arr: any[], level: number) {
+    const res = new Array(arr.length);
+    for (let i = 0; i < arr.length; i++) {
+        res[i] = clone(arr[i], level);
+    }
+    return res;
+}
+
+export function cloneObject(obj: any, level: number) {
+    const res: any = {};
+    for (const key in obj) {
+        res[key] = clone(obj[key], level);
+    }
+    return res;
+}
