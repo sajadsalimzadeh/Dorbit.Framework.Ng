c9385c7fc46e47167635b1a7aa932d777f701398
sajad <salimzadehsajad@gmail.com>
2023-03-26 14:33:30 +0330

    add object utils

---
+++ src/app/core/utils/object.ts
@@ -0,0 +1,11 @@
+export class ObjectUtility {
+
+    static deleteUndefinedProperty(obj: any): any {
+        const result: any = {};
+        for (const key in obj) {
+            const value = obj[key];
+            if (typeof value !== 'undefined') result[key] = value;
+        }
+        return result;
+    }
+}
