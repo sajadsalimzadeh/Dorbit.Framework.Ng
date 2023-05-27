9099fd0ff3b527ca28195cb9cb92aa1ef7c6f3a1
sajad <salimzadehsajad@gmail.com>
2023-05-27 21:28:41 +0330

    add tree utils

--- projects/core/src/utils/index.ts
+++ projects/core/src/utils/index.ts
@@ -1,3 +1,4 @@
 export * from './object';
 export * from './clone';
 export * from './datetime';
+export * from './tree';
--- projects/core/src/utils/tree.ts
+++ projects/core/src/utils/tree.ts
@@ -1,19 +1,19 @@
 
 interface TreeIterateOptions<T> {
-  prevAction?: (item: T) => void;
-  postAction?: (item: T) => void;
+  prevAction?: (item: T, parent?: T) => void;
+  postAction?: (item: T, parent?: T) => void;
 }
 
 export class TreeUtil {
-  static iterate<T extends {children?: T[]} = any>(items: T[], options: TreeIterateOptions<T>) {
+  static iterate<T extends {children?: T[]} = any>(items: T[], options: TreeIterateOptions<T>, parent?: T) {
 
     items?.forEach(x => {
       if(options.prevAction) {
-        options.prevAction(x);
+        options.prevAction(x, parent);
       }
-      this.iterate(x.children, options);
+      if(x.children) this.iterate(x.children, options, x);
       if(options.postAction) {
-        options.postAction(x);
+        options.postAction(x, parent);
       }
     })
   }
