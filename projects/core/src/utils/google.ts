ef22b15718526362689ee4ece50728e5de4fe7b8
sajadsalimzadeh <salimzadehsajad@gmail.com>
2023-11-12 13:56:22 +0330

    improve: add open in google map link to diagnostic report lat lng
    issue:MPN-1005

--- projects/core/src/utils/google.ts
+++ projects/core/src/utils/google.ts
@@ -1,5 +1,9 @@
 export class GoogleUtil {
-  static directionTo(latLng: {lat: number, lng: number}) {
-    return `https://www.google.com/maps/dir/?api=1&travelmode=driving&layer=traffic&destination=${latLng.lat},${latLng.lng}`
+  static directionTo(lat: number, lng: number) {
+    return `https://www.google.com/maps/dir/?api=1&travelmode=driving&layer=traffic&destination=${lat},${lng}`
+  }
+
+  static open(lat: number, lng: number) {
+    window.open(`https://www.google.com/maps/place/${lat},${lng}/@${lat},${lng},17z`, '_blank')
   }
 }
