4061143990eb6ba25c49b80b88a126a0079da275
sajadsalimzadeh <salimzadehsajad@gmail.com>
2024-03-18 18:50:39 +0330

    improve change logs component

--- projects/core/src/utils/string.ts
+++ projects/core/src/utils/string.ts
@@ -23,4 +23,14 @@ export class StringUtil {
 
     return result;
   }
+  static parseVersion(version: string, sectionValue = 100) {
+    let value = 1;
+    let result = 0;
+    const sections = version.split('.')
+    sections.forEach(x => {
+      result += result * value + (+x);
+      value *= sectionValue;
+    })
+    return result;
+  }
 }
