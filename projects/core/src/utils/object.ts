c4bfacd591509f8f31c6ac1b37b2d43c4bd0f2d5
sajad <salimzadehsajad@gmail.com>
2024-05-10 02:00:28 +0330

    improve: object util

--- projects/core/src/utils/object.ts
+++ projects/core/src/utils/object.ts
@@ -20,11 +20,20 @@ export class ObjectUtil {
     }
   }
 
-  static deleteUndefinedProperty(obj: any): any {
+  static getObjectWithNotUndefinedProperty(obj: any): any {
     const result: any = {};
     for (const key in obj) {
       const value = obj[key];
       if (typeof value !== 'undefined') result[key] = value;
+    }
+    return result;
+  }
+
+  static getObjectWithNotNullProperty(obj: any): any {
+    const result: any = {};
+    for (const key in obj) {
+      const value = obj[key];
+      if (value !== null) result[key] = value;
     }
     return result;
   }
