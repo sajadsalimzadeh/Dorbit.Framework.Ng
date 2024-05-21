fedf1f69f3aa2f6b0d309bb8c4ede76bf1e215f0
sajad <salimzadehsajad@gmail.com>
2024-05-22 01:29:26 +0330

    improve: notification

--- projects/core/src/repositories/notification.repository.ts
+++ projects/core/src/repositories/notification.repository.ts
@@ -24,7 +24,19 @@ export class NotificationRepository extends BaseApiRepository {
     return this.http.get<QueryResult<NotificationRecord[]>>('');
   }
 
-  send(req: {userIds: string[], notification: NotificationDto, test: boolean}) {
+  last() {
+    return this.http.get<QueryResult<NotificationRecord>>('Last');
+  }
+
+  send(req: {userIds: string[], notification: NotificationDto}) {
     return this.http.post<CommandResult>('', req);
+  }
+
+  empty() {
+    return this.http.post<CommandResult>('Empty', {});
+  }
+
+  delete(id: string) {
+    return this.http.delete<CommandResult>('' + id);
   }
 }
