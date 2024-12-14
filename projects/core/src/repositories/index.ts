6a900282d944b8c9a60f2e40612ee3196670f795
sajad salimzadeh <salimzadehsajad@gmail.com>
2024-12-14 20:51:06 +0330

    bug: remove wrapper repository

--- projects/core/src/repositories/index.ts
+++ projects/core/src/repositories/index.ts
@@ -5,4 +5,3 @@ export * from './base-write.repository';
 export * from './notification.repository';
 export * from './file.repository';
 export * from './job.repository';
-export * from './wrap.repository';
