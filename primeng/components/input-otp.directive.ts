588b7f4f91e77f8026fea96315b2341ca7f3c5ef
sajad <salimzadehsajad@gmail.com>
2025-10-30 17:38:54 +0330

    improve: p input otp

--- primeng/components/input-otp.directive.ts
+++ primeng/components/input-otp.directive.ts
@@ -2,17 +2,17 @@ import { AfterViewInit, Directive, ElementRef, Input } from "@angular/core";
 
 @Directive({
     standalone: true,
-    selector: "p-input-otp[inputmode]",
+    selector: "p-input-otp[inputMode]",
 })
 export class InputOtpDirective implements AfterViewInit {
-    @Input() inputmode: "numeric" | "tel" = "numeric";
+    @Input() inputMode: "numeric" | "tel" = "numeric";
 
     constructor(private el: ElementRef) {}
 
     ngAfterViewInit(): void {
         this.el.nativeElement.querySelector('input').setAttribute("autocomplete", 'one-time-code');
         this.el.nativeElement.querySelectorAll('input').forEach((input: HTMLInputElement) => {
-            input.setAttribute("inputmode", this.inputmode);
+            input.setAttribute("inputmode", this.inputMode);
         });
     }
 }
