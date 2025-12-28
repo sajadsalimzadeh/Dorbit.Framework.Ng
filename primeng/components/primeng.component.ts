b212abb6149494cba7eb633db22fad73217b9205
sajad salimzadeh <salimzadehsajad@gmail.com>
2025-12-28 13:31:28 +0330

    improve: sevices cache field

--- primeng/components/primeng.component.ts
+++ primeng/components/primeng.component.ts
@@ -15,50 +15,50 @@ export abstract class PrimengComponent implements OnInit, OnChanges, OnDestroy {
     protected selectedItem: any;
     protected dialogs: { [key: string]: boolean } = {};
     protected loadings: { [key: string]: boolean } = {};
-    protected services: any = {};
+    protected servicesCache: any = {};
 
     constructor(protected injector: Injector) {
 
     }
 
     protected get messageService(): MessageService {
-        return this.services['MessageService'] ??= this.injector.get(MessageService);
+        return this.servicesCache['MessageService'] ??= this.injector.get(MessageService);
     }
 
     protected get fileRepository(): FileRepository {
-        return this.services['FileRepository'] ??= this.injector.get(FileRepository);
+        return this.servicesCache['FileRepository'] ??= this.injector.get(FileRepository);
     }
 
     protected get translateService(): TranslateService {
-        return this.services['TranslateService'] ??= this.injector.get(TranslateService);
+        return this.servicesCache['TranslateService'] ??= this.injector.get(TranslateService);
     }
 
     protected get router(): Router {
-        return this.services['Router'] ??= this.injector.get(Router);
+        return this.servicesCache['Router'] ??= this.injector.get(Router);
     }
 
     protected get elementRef(): ElementRef {
-        return this.services['ElementRef'] ??= this.injector.get(ElementRef);
+        return this.servicesCache['ElementRef'] ??= this.injector.get(ElementRef);
     }
 
     protected get changeDetectorRef(): ChangeDetectorRef {
-        return this.services['ChangeDetectorRef'] ??= this.injector.get(ChangeDetectorRef);
+        return this.servicesCache['ChangeDetectorRef'] ??= this.injector.get(ChangeDetectorRef);
     }
 
     protected get confirmationService(): ConfirmationService {
-        return this.services['ConfirmationService'] ??= this.injector.get(ConfirmationService);
+        return this.servicesCache['ConfirmationService'] ??= this.injector.get(ConfirmationService);
     }
 
     protected get location(): Location {
-        return this.services['Location'] ??= this.injector.get(Location);
+        return this.servicesCache['Location'] ??= this.injector.get(Location);
     }
 
     protected get route(): ActivatedRoute {
-        return this.services['ActivatedRoute'] ??= this.injector.get(ActivatedRoute);
+        return this.servicesCache['ActivatedRoute'] ??= this.injector.get(ActivatedRoute);
     }
 
     protected get ngZone(): NgZone {
-        return this.services['NgZone'] ??= this.injector.get(NgZone);
+        return this.servicesCache['NgZone'] ??= this.injector.get(NgZone);
     }
 
     protected tapLoading<T>(key: string) {
