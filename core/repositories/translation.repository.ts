519ca8948c488ffa7a4ce7755cdaee9a16eb2cc1
sajad <salimzadehsajad@gmail.com>
2026-01-03 23:15:44 +0330

    improve: translate service repository

---
+++ core/repositories/translation.repository.ts
@@ -0,0 +1,19 @@
+import { Injectable, Injector } from "@angular/core";
+import { Translation } from "@app/contracts/translation";
+import { BASE_URL_FRAMEWORK } from "@framework/configs";
+import { QueryResult } from "@framework/contracts";
+import { BaseApiRepository } from "@framework/repositories";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class TranslationRepository extends BaseApiRepository {
+
+    constructor(injector: Injector) {
+        super(injector, injector.get(BASE_URL_FRAMEWORK) , 'Translations');
+    }
+
+    translateAll(locale: string, keys: string[]) {
+        return this.http.post<QueryResult<Translation[]>>(``, keys, {params: {locale}});
+    }
+}
