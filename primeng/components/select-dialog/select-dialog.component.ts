e1607206d4c404f2f190e1a1026cae2632204fbc
sajad salimzadeh <salimzadehsajad@gmail.com>
2026-05-30 17:36:03 +0330

    improve: select dialog

--- primeng/components/select-dialog/select-dialog.component.ts
+++ primeng/components/select-dialog/select-dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component, ContentChild, ElementRef, forwardRef, Input, TemplateRef } from '@angular/core';
+import { Component, computed, ContentChild, ContentChildren, ElementRef, forwardRef, HostBinding, HostListener, Input, Output, QueryList, signal, TemplateRef } from '@angular/core';
 import { PrimengControlComponent } from '../primeng-control.component';
 import { NG_VALUE_ACCESSOR } from '@angular/forms';
 
@@ -16,12 +16,54 @@ import { NG_VALUE_ACCESSOR } from '@angular/forms';
     ]
 })
 export class SelectDialogComponent extends PrimengControlComponent {
-    @Input() items: any[] = [];
+    @Input() options: any[] = [];
     @Input() optionLabel?: string;
     @Input() optionValue?: string;
+    @Input() filter: boolean = false;
+    @Input() showClear: boolean = false;
+    @Input() display: 'list' | 'grid' = 'list';
+    @Input() size: 'xs' | 'sm' | 'md' | 'lg' | 'xl' = 'md';
 
-    @ContentChild('selectedItem', { read: ElementRef }) selectedItemTpl?: TemplateRef<any>;
-    @ContentChild('item', { read: ElementRef }) itemTpl?: TemplateRef<any>;
+    @Input() gridColumns: number = 4;
 
-    protected isShowDialog = false;
+    @HostListener('click') onClick() {
+        this.isShowDialog.set(true);
+    }
+
+    @ContentChildren(TemplateRef) set templates(templates: QueryList<TemplateRef<any>>) { 
+     templates.forEach((template:any) => {
+        const localNames = template?._declarationTContainer?.localNames ?? [];
+        if (localNames.includes('selectedItem')) {
+            this.selectedItemTpl = template;
+        }
+        if (localNames.includes('item')) {
+            this.itemTpl = template;
+        }
+     });
+    }
+
+    selectedItemTpl?: TemplateRef<any>;
+    itemTpl?: TemplateRef<any>;
+
+    protected isShowDialog = signal(false);
+    protected filterValue = signal('');
+
+    optionsFiltered = computed(() => {
+        if (this.filter) {
+            return this.options.filter(option => (this.optionLabel ? option[this.optionLabel] : option)?.toLowerCase().includes(this.filterValue().toLowerCase())).slice(0, 100);
+        }
+        return this.options;
+    });
+
+    override ngOnInit() {
+        super.ngOnInit();
+
+        this.filterValue.set('');
+    }
+
+    select(item: any) {
+        this.value = (item ? (this.optionValue ? item[this.optionValue] : item) : null);
+        this.onChange(this.value);
+        this.isShowDialog.update(value => !value);
+    }
 }
