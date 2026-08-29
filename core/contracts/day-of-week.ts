b01893574324261cef293fe55b51c9a720bf7c9c
sajad salimzadeh <salimzadehsajad@gmail.com>
2026-08-29 17:00:00 +0330

    improve: add day of week

---
+++ core/contracts/day-of-week.ts
@@ -0,0 +1,17 @@
+export enum DayOfWeek
+{
+  /// <summary>Indicates Sunday.</summary>
+  Sunday,
+  /// <summary>Indicates Monday.</summary>
+  Monday,
+  /// <summary>Indicates Tuesday.</summary>
+  Tuesday,
+  /// <summary>Indicates Wednesday.</summary>
+  Wednesday,
+  /// <summary>Indicates Thursday.</summary>
+  Thursday,
+  /// <summary>Indicates Friday.</summary>
+  Friday,
+  /// <summary>Indicates Saturday.</summary>
+  Saturday,
+}
